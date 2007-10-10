6da84022a4669ee42b74e71c18df822ebfa95db7
amallavarapu <amallavarapu>
2007-10-10 21:12:22 +0000

    now correctly checksout lisa from sourceforge

--- get-cvs-modules.bat
+++ get-cvs-modules.bat
@@ -1,4 +1,5 @@
+echo off
 echo This script checks out libraries required by little b from SourceForge.net.  To work, cvs.exe (Concurrent Version System) must be in the path.
 
-echo cvs -d:pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa co -P lisa
+cvs -d:pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa co -P lisa
 cvs -d:pserver:anonymous@graph-tools.cvs.sourceforge.net:/cvsroot/graph-tools co -P graph-tools
