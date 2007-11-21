1be0c7c12ae7fafa3e038c1bab2b745eb6c9c4ce
amallavarapu <amallavarapu>
2007-11-21 14:17:24 +0000

    does a fresh anonymous checkout of code to build a working little b source folder

---
+++ release/make-release.bat
@@ -0,0 +1,10 @@
+rmdir /S /Q b1
+cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb checkout -P b1
+cd b1
+cvs -d :pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa checkout -Pd lisa
+cvs -d :pserver:anonymous@graph-tools.cvs.sourceforge.net:/cvsroot/graph-tools checkout -dP graph-tools
+cd libraries
+cvs -d :ext:am116@orchestra.med.harvard.edu:/cvs/littleb checkout segment-polarity
+cvs -d :ext:am116@orchestra.med.harvard.edu:/cvs/littleb checkout scaffold
+cvs -d :ext:am116@orchestra.med.harvard.edu:/cvs/littleb checkout multisite
+cd ../..
