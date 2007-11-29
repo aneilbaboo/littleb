a21b5d5312ef42ff9003941f116d96c3a760dab8
amallavarapu <amallavarapu>
2007-11-29 18:41:10 +0000

    modified specification: SHC now binds from :inner compartment

--- libraries/examples/example5-complexes.lisp
+++ libraries/examples/example5-complexes.lisp
@@ -26,14 +26,14 @@
                      ->> [[egfr 1][egf 1]]})
 
 ;; SHC binds to EGFR when located in the C2 membrane
-;;;; (define shc-egfr-binding {[egfr * _ *] + [shc] @ :outer ;; Note: [shc] is shorthand for [shc _]
-;;;;                           ->>
-;;;;                           [[egfr * 1 *][shc 1]]})
+(define shc-egfr-binding {[egfr * _ *] + [shc] @ :inner ;; Note: [shc] is shorthand for [shc _]
+                           ->>
+                           [[egfr * 1 *][shc 1]]})
 
 
 ;; set the rate functions:
 egf-binding.(set-rate-function 'mass-action 2)
-;;shc-egfr-binding.(set-rate-function 'mass-action 2)
+shc-egfr-binding.(set-rate-function 'mass-action 2)
 
 
 ;;;
