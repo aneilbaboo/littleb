a6e1124e7a5c9caa95a071cb07896db79abbfc36
amallavarapu <amallavarapu>
2007-12-21 16:45:01 +0000

    *** empty log message ***

--- get-cvs-modules.bat
+++ get-cvs-modules.bat
@@ -2,4 +2,4 @@ echo off
 echo This script checks out libraries required by little b from SourceForge.net.  To work, cvs.exe (Concurrent Version System) must be in the path.
 
 cvs -d:pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa co -P lisa
-cvs -d:pserver:anonymous@graph-tools.cvs.sourceforge.net:/cvsroot/graph-tools co -P graph-tools
+cvs -d:pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
--- get-cvs-modules.sh
+++ get-cvs-modules.sh
@@ -1,2 +1,2 @@
 cvs -d:pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa co -P lisa
-cvs -d:pserver:anonymous@graph-tools.cvs.sourceforge.net:/cvsroot/graph-tools co -P graph-tools
+cvs -d:pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
