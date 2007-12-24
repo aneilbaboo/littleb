7e89edaae586112fbcbded461ba596b4881977ad
amallavarapu <amallavarapu>
2007-12-24 02:45:46 +0000

    get-cvs-modules scripts now cvs export lisa rather than checkout    (before this, cvs tag failed because no lisa write access) changelog and release notes for V1-5-6

--- get-cvs-modules.sh
+++ get-cvs-modules.sh
@@ -1,2 +1,4 @@
-cvs -d :pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa co -P lisa
+rm -r lisa
+cvs -d :pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa export -r RELEASE_3_2 lisa
+rm -r graph-tools
 cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
