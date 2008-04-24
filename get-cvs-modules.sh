c8fec410a4673bb7a3f0677447142b903d9f7c63
amallavarapu <amallavarapu>
2008-04-24 21:01:54 +0000

    now applies lisa inter-pattern-filter patch

--- get-cvs-modules.sh
+++ get-cvs-modules.sh
@@ -2,3 +2,7 @@ rm -r lisa
 cvs -d :pserver:anonymous@lisa.cvs.sourceforge.net:/cvsroot/lisa export -r RELEASE_3_2 lisa
 rm -r graph-tools
 cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
+cp support/lisa3_2-inter-pattern-filter.patch lisa
+cd lisa
+patch -p0 < lisa3_2-inter-pattern-filter.patch
+cd .
