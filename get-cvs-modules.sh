2f50c52695a4d7f93c9592f942bf036712d35b4d
amallavarapu <amallavarapu>
2008-09-06 02:46:46 +0000

    now exports slime tag=SLIME-2-1

--- get-cvs-modules.sh
+++ get-cvs-modules.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
 rm -r lisa
-cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P lisa
+if [ -z $1 ]; then
+   cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P lisa
+else
+   cvs -d :ext:${1}@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P lisa
+fi
 rm -r graph-tools
-cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
-rmdir slime
-cvs -d :pserver:anonymous:anonymous@common-lisp.net:/project/slime/cvsroot co slime
+if [ -z $1 ]; then
+   cvs -d :pserver:anonymous@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
+else
+   cvs -d :ext:${1}@littleb.cvs.sourceforge.net:/cvsroot/littleb co -P graph-tools
+fi
+rm -r slime
+cvs -d :pserver:anonymous:anonymous@common-lisp.net:/project/slime/cvsroot export -r SLIME-2-1 slime
